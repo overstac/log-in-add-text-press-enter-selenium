4822b5da7a9291af5c3f1a774a2a64a0b0eb1be5
Catalin Pintilei <105587254+overstac@users.noreply.github.com>
2022-05-15 15:33:31 +0000

    Exercitiu, log in , push button, copy text

--- main.py
+++ main.py
@@ -15,14 +15,18 @@ def get_drvier():
   driver = webdriver.Chrome(options=options)
   driver.get("https://automated.pythonanywhere.com/login/")
   return driver
+  
+def clean_text(text):
+  output= float(text.split(": ")[1])
+  return output
 
 def main():
   driver = get_drvier()
   driver.find_element(by="id", value="id_username").send_keys("automated")
-  time.sleep(2)
   driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
   driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-  print(driver.current_url)
+  element=driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+  time.sleep(2)
+  return clean_text(element.text)
   
-
 print(main())
